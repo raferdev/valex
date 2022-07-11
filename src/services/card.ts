29aa57d8476ed83551e99a37992741ccbbcc506e
Rafael <rafinha_fern@hotmail.com>
2022-07-11 14:45:33 -0300

    feat: short card name in create card service

--- src/services/card.ts
+++ src/services/card.ts
@@ -1,13 +1,26 @@
 import { TransactionTypes } from "../repositories/cardRepository.js";
-import { faker } from '@faker-js/faker';
+import { faker, StringColorFormat } from '@faker-js/faker';
 import dayjs from 'dayjs';
 import Repositories from "../repositories/index.js";
 
 async function create(employee:any,company:any,type:TransactionTypes) {
+    let shortCardName = '';
+    const employeeNameArr = employee.fullName.split(' ');
+    employeeNameArr.forEach( (name:string, index:number)  => {
+        if(index === 0) {
+           return shortCardName += name
+        }
+        if(index === employeeNameArr.length-1) {
+           return shortCardName += ` ${name}`
+        }
+        if(name.length >= 3) {
+           return shortCardName += ` ${name[0].toLocaleUpperCase()}`;
+        }
+    })
     const card = {
     employeeId: employee.id,
     number: faker.random.numeric(16),
-    cardholderName: employee.fullName,
+    cardholderName: shortCardName,
     securityCode: faker.random.numeric(3),
     expirationDate: dayjs().format('MM/YY'),
     password: null,
